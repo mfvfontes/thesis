097f64bcd689baa44195d0f7483ce39ba9dee416
Marcio Fontes <ei12183@fe.up.pt>
2016-11-24 11:41:45 +0000

    speed

---
+++ scripts/speed_per_edge/get_speed_per_edge_hour.R
@@ -0,0 +1,36 @@
+require("RPostgreSQL")
+
+pw <- {
+  "y7dWwByZLWso"
+}
+
+drv <- dbDriver("PostgreSQL")
+
+con <- dbConnect(drv, dbname = "sensemycity",
+                 host = "localhost", port = 15432,
+                 user = "marciofontes", password = pw)
+
+df_speed_per_edge_hour <- data.frame()
+
+df_speed_per_edge_hour <- dbGetQuery(con, "SELECT way_id, session_id, hour, total_distance, total_time, total_distance / total_time AS speed
+                                FROM (
+                                SELECT way_id, session_id, extract(hour from to_timestamp(seconds)) AS hour, SUM(ST_Distance(geo, nextGeo)) AS total_distance, (MAX(seconds+(millis/1000.0)) - MIN(seconds+(millis/1000.0))) AS total_time
+                                FROM (
+                                SELECT  T1.way_id, T1.session_id, T1.seconds, T1.millis, T1.geo,
+                                (SELECT T2.geo 
+                                FROM marciofontes.osmlocation_april T2
+                                WHERE (T2.seconds > T1.seconds
+                                OR (T2.seconds = T1.seconds
+                                AND T2.millis > T1.millis))
+                                AND T1.session_id = T2.session_id
+                                AND T1.way_id = T2.way_id
+                                ORDER BY T2.seconds, T2.millis
+                                LIMIT 1) nextGeo
+                                FROM marciofontes.osmlocation_april T1) T3
+                                WHERE (extract(dow from to_timestamp(seconds)) != 0 AND extract(dow from to_timestamp(seconds)) != 6)
+                                GROUP BY session_id, way_id, hour
+                                ) T4
+                                WHERE total_time > 0
+                                ORDER BY speed DESC")
+
+save(df_speed_per_edge_hour, file = "df_speed_per_edge_hour.Rda")
